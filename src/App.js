c2248397e7207126fd748a3a19c60758f26c1265
Zeztron <pateltharsh@gmail.com>
2019-01-26 13:02:00 -0500

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello</h1>
+      <Navbar />
     );
   }
 }
